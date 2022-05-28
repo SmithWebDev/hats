37b55c695a1e5b0880287d075867754041bdf1f0
SmithWebDev <smithwebdd@gmail.com>
2022-05-28 15:01:04 -0400

    updated application.js to ensure Turbo, StimulusReflex, and CableReady usage along with JSON serializer via CableCar

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,16 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
+
+// Connects ActionCable consumer, with MRUJS wrapping CableCar for CableReady
+// usage
+import consumer from "./channel/consumer";
+import CableReady from "cable_ready";
+import mrujs from "mrujs";
+import { CableCar } from "mrujs/plugins";
+
+mrujs.start({
+  plugins: [
+    new CableCar(CableReady)
+  ]
+})
